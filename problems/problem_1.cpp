771b7db095d3c45df0cc929b2451269dceab5094
Abdurahmon Abdurazzoqov <147364902+edzoyd@users.noreply.github.com>
2023-11-14 11:05:47 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,14 +1,5 @@
 float problemSolution1(float consumed_water) {
     float cost;
-   if (consumed_water <= 30) {
-        cost += consumed_water * 0.4;
-    } else if (consumed_water <= 50) {
-        cost += 30 * 0.4 + (consumed_water - 30) * 0.12;
-    } else if (consumed_water <= 60) {
-        cost += 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
-    } else {
-        cost += 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
-    }
 
     return cost;
 }
